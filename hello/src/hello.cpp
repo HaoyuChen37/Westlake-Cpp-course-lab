61edbcb0495772ae67db4e846023ecd6ceb699a4
陈浩宇 <chenhaoyu@westlake.edu.cn>
2025-02-21 10:03:59 +0800

    initialization of the repo

---
+++ hello/src/hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string>
+#include <sstream>
+#include <vector>
+
+
+
+int main(int argc, char **argv) {
+  
+    std::cout<<"hello world"<<std::endl;
+    
+    
+    
+    return 0;
+}
